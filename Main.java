ba4ecffcb32bd84cf98d5769268481941300d7e9
Jamiul Abedin Shovon <51233770+jamiulshovon@users.noreply.github.com>
2023-05-31 01:06:39 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main  {
     public static void main(String[] args) {
             spider o= new spider();
             o.eat();
-    Fish f= new Fish("123");
+    Fish f= new Fish("0000");
     f.play();
 
     }
